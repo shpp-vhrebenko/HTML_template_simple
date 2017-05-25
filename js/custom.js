4a2423f2cacbf55070302d023afb9816481d2282
shpp-vhrebenko <vhrebenko@gmail.com>
2017-05-25 23:30:46 +0300

    add wow.js

--- js/custom.js
+++ js/custom.js
@@ -213,6 +213,22 @@ $(function() {
 
 
 
+    /* WOW.js https://github.com/matthieua/WOW
+    ========================*/
+
+    var wow = new WOW(
+        {
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       150,          // distance to the element when triggering the animation (default is 0)
+            mobile:       false,       // trigger animations on mobile devices (default is true)
+            live:         true,       // act on asynchronously loaded content (default is true)
+            scrollContainer: null // optional scroll container selector, otherwise use window
+        }
+    );
+    wow.init();
+
+
 
 
 
